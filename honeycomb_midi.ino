678df94badb7783e237e27e0e10e3510fb25979f
Andrew Burns <aburnsni@gmail.com>
2018-03-12 09:58:04 +0000

    Moved MIDI to Serial 1 and switched on debugging messages on Serial

--- honeycomb_midi.ino
+++ honeycomb_midi.ino
@@ -37,84 +37,95 @@ uint16_t lasttouched = 0;
 uint16_t currtouched = 0;
 
 
-  //MIDI Setup
-  int* song[] = {NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4, NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5};
-  int midiChannel[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}; // midi channel for each button
-  int instruments[] = {102, 999, 999, 999, 999, 999, 999, 999, 999, 999 /*Drums*/, 999, 999, 999, 999, 999, 999};
-  MIDI_CREATE_INSTANCE(HardwareSerial, Serial, MIDI);
+//MIDI Setup
+int* song[] = {NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4, NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5};
+int midiChannel[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}; // midi channel for each button
+int instruments[] = {102, 999, 999, 999, 999, 999, 999, 999, 999, 999 /*Drums*/, 999, 999, 999, 999, 999, 999};
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI1);  //  Serial1 for pro micro Serial output
 
-  void setup() {
+void setup() {
 
-    MIDI.begin();
+  MIDI1.begin();
     Serial.begin(115200);  // needed for hairless midi
-    //while (!Serial); // wait for serial attach
+  // while (!Serial); // wait for serial attach
     delay(2000);
 
-    //  Serial.println();
-    //  Serial.println("Initializing...");
-    //  Serial.flush();
-    if (!cap.begin(0x5A)) {
-      Serial.println("MPR121 not found, check wiring?");
-      while (1);
-    }
-    strip.Begin();
+      Serial.println();
+      Serial.println("Initializing...");
+      Serial.flush();
+
+  strip.Begin();
+  strip.Show();
+    Serial.println();
+    Serial.println("Running...");
+
+  for (int led = 0; led < PixelCount; led++) {
+    delay(100);
+    strip.SetPixelColor(led, RgbColor(colors[led][0]* brightness, colors[led][1]* brightness, colors[led][2]* brightness));
     strip.Show();
-    //  Serial.println();
-    //  Serial.println("Running...");
+    // strip.SetPixelColor(i, black);
+    //  Serial.println("Set LED blue");
+  }
 
-    for (int led = 0; led < PixelCount; led++) {
-      delay(100);
-      strip.SetPixelColor(led, RgbColor(colors[led][0], colors[led][1], colors[led][2]));
-      strip.Show();
-      // strip.SetPixelColor(i, black);
-      //  Serial.println("Set LED blue");
+//  if (!cap.begin(0x5A)) {  // NOT WORKING!!!
+//    // Serial.println("MPR121 not found, check wiring?");
+//    for (uint8_t i = 0; i < 3; i++) {
+//      strip.SetPixelColor(0, black);
+//      strip.Show();
+//      delay(200);
+//      strip.SetPixelColor(0, red);
+//      strip.Show();
+//      delay(200);
+//    }
+//    while (1);
+//  }
+
+  MIDIsoftreset();  // Midi Reset
+  delay(200);
+  for (uint8_t i = 0; i < 16; i++) {  // Set instruments for all 16 MIDI channels
+    if (instruments[i] < 128) {
+      MIDI1.sendProgramChange(instruments[i], i + 1);
     }
-    MIDIsoftreset();  // Midi Reset
-    delay(200);
-    for (uint8_t i = 0; i < 16; i++) {  // Set instruments for all 16 MIDI channels
-      if (instruments[i] < 128) {
-        MIDI.sendProgramChange(instruments[i], i + 1);
-      }
+  }
+}
+
+void loop() {
+  currtouched = cap.touched();
+
+  for (uint8_t i = 0; i < 12; i++) {
+    // it if *is* touched and *wasnt* touched before, alert!
+    if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) ) {
+      MIDI1.sendNoteOn(song[i], 100, midiChannel[i]);
+      turnOnLED(i);
+                  Serial.print("Pressed ");
+                  Serial.println (i);
+    }
+    if (!(currtouched & _BV(i)) && (lasttouched & _BV(i)) ) {
+      MIDI1.sendNoteOff(song[i], 100, midiChannel[i]);
+      turnOffLEDs(i);
+                  Serial.print("Released ");
+                  Serial.println (i);
     }
   }
+  lasttouched = currtouched;
+  delay(1);
+}
 
-  void loop() {
-    currtouched = cap.touched();
+void turnOnLED( int led ) {
+  strip.SetPixelColor(led, white);
+  strip.Show();
+  delay(10);
+}
+void turnOffLEDs( int led ) {
+  strip.SetPixelColor(led, RgbColor(colors[led][0]* brightness, colors[led][1]* brightness, colors[led][2]* brightness));
+  strip.Show();
+  delay(10);
+}
 
-    for (uint8_t i = 0; i < 12; i++) {
-      // it if *is* touched and *wasnt* touched before, alert!
-      if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) ) {
-        MIDI.sendNoteOn(song[i], 100, midiChannel[i]);
-        turnOnLED(i);
-        //      Serial.print("Pressed ");
-        //      Serial.println (i);
-      }
-      if (!(currtouched & _BV(i)) && (lasttouched & _BV(i)) ) {
-        MIDI.sendNoteOff(song[i], 100, midiChannel[i]);
-        turnOffLEDs(i);
-        //      Serial.print("Released ");
-        //      Serial.println (i);
-      }
-    }
-    lasttouched = currtouched;
-    delay(1);
-  }
-
-  void turnOnLED( int led ) {
-    strip.SetPixelColor(led, white);
-    strip.Show();
-    delay(10);
-  }
-  void turnOffLEDs( int led ) {
-strip.SetPixelColor(led, RgbColor(colors[led][0], colors[led][1], colors[led][2]));
-    strip.Show();
-    delay(10);
-  }
-
-  void MIDIsoftreset()  // switch off ALL notes on channel 1 to 16
+void MIDIsoftreset()  // switch off ALL notes on channel 1 to 16
+{
+  for (int channel = 0; channel < 16; channel++)
   {
-    for (int channel = 0; channel < 16; channel++)
-    {
-      MIDI.sendControlChange(123, 0, channel);
-    }
+    MIDI1.sendControlChange(123, 0, channel);
   }
+}
